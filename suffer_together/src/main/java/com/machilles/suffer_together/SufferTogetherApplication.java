237c970373fc467211563f25c1713e9cf5fff044
M-Achilles <marcoachilles@gmx.de>
2020-11-01 01:36:41 +0100

    suffer_together: Import module
    Import generated module suffer_together. Module was generated by spring initializr

---
+++ suffer_together/src/main/java/com/machilles/suffer_together/SufferTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.machilles.suffer_together;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SufferTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SufferTogetherApplication.class, args);
+	}
+
+}
---
+++ suffer_together/src/test/java/com/machilles/suffer_together/SufferTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.machilles.suffer_together;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SufferTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

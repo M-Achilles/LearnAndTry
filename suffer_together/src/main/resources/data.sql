ccd193ff4d5a87b125da4e18f17ff21ff2f8fced
M-Achilles <marcoachilles@gmx.de>
2020-11-16 21:52:22 +0100

    Create data.sql
    Create data.sql for some example configurations

---
+++ suffer_together/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into TodoListTypes (TodoListType_Name) values ('Shopping-List');
+
+insert into TodoTypes (TodoType_Name) values ('Purchase');
+insert into TodoTypes (TodoType_Name) values ('Replacement');
